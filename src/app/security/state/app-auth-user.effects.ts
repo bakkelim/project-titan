3975cde54c27334947904f6fa029a0b2fbbfb926
Marius Bakkeli <marius_bakkeli@hotmail.com>
2018-07-16 09:35:15 +0200

    added redirect on login-logout

--- src/app/security/state/app-auth-user.effects.ts
+++ src/app/security/state/app-auth-user.effects.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 import { AppAuthUserActions, AppAuthUserActionTypes, LoginAppAuthUser, LoginAppAuthUserSuccess, LoginAppAuthUserError } from './app-auth-user.actions';
 import { of } from 'rxjs';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap, catchError, tap } from 'rxjs/operators';
 import { SecurityService } from '../services/security.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AppAuthUserEffects {
@@ -14,15 +15,30 @@ export class AppAuthUserEffects {
     .pipe(
       map((action: LoginAppAuthUser) => action.payload),
       switchMap((payload) =>
-      this.secruityService.login(payload.username, payload.password).pipe(
-        map(data => new LoginAppAuthUserSuccess(data)),
-        catchError(err => of(new LoginAppAuthUserError(err)))
+        this.secruityService.login(payload.username, payload.password).pipe(
+          map(data => new LoginAppAuthUserSuccess(data)),
+          catchError(err => of(new LoginAppAuthUserError(err)))
+        )
       )
-    )
-  );
+    );
+
+  @Effect({ dispatch: false })
+  loginSuccess$ = this.actions$
+    .ofType(AppAuthUserActionTypes.LoginAppAuthUserSuccess)
+    .pipe(
+      tap(() => this.router.navigate(['/']))
+    );
+
+  @Effect({ dispatch: false })
+  logout$ = this.actions$
+    .ofType(AppAuthUserActionTypes.LogoutAppAuthUser)
+    .pipe(
+      tap(() => this.router.navigate(['/login']))
+    );
 
   constructor(
     private actions$: Actions,
     private secruityService: SecurityService,
+    private router: Router,
   ) { }
 }
